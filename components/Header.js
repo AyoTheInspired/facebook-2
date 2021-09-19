bbe567166570d70cb50cc73af5b7b2c317d2d10e
Ayo Abimbola <ayoinspired@gmail.com>
2021-09-19 02:20:31 +0100

    Added styling on header

--- components/Header.js
+++ components/Header.js
@@ -19,7 +19,7 @@ import HeaderIcon from "./HeaderIcon";
 
 function Header() {
 	return (
-		<div>
+		<div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
 			<div className="flex items-center">
 				<Image
 					src="https://links.papareact.com/5me"
@@ -27,12 +27,13 @@ function Header() {
 					height={40}
 					layout="fixed"
 				/>
+				changes here
 				<div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
 					<SearchIcon className="h-6 text-gray-600" />
 					<input
 						type="text"
 						placeholder="Search Facebook"
-						className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+						className="flex ml-2 items-center bg-transparent outline-none text-shrink placeholder-gray-500"
 					/>
 				</div>
 			</div>
